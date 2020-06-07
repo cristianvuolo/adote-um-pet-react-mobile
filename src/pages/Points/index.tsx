45001be78108511f4c7a6ac3d1dced9b89e5cf2f
Cristian Vuolo <cristianvuolo@gmail.com>
2020-06-07 11:36:13 -0300

    add API to Points

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,95 +1,161 @@
-import React from 'react';
-import {View, StyleSheet, TouchableOpacity, Text, ScrollView, Image, SafeAreaView} from "react-native";
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, TouchableOpacity, Text, ScrollView, Image, SafeAreaView, Alert} from "react-native";
 import Constants from "expo-constants";
 import {Feather as Icon} from '@expo/vector-icons';
 import {useNavigation} from '@react-navigation/native'
 import MapView, {Marker} from 'react-native-maps'
+import api from '../../services/api';
+import * as Location from 'expo-location';
+
+interface Item {
+    id: number;
+    name: string;
+}
+
+interface Point {
+    id: number;
+    name: string;
+    image: string;
+    latitude: number;
+    longitude: number;
+}
 
 const Points = () => {
     const navigation = useNavigation();
+    const [selectedItems, setSelectedItems] = useState<number[]>([])
+    const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+    const [points, setPoints] = useState<Point[]>([]);
 
     function handleNavigateBack() {
         navigation.goBack();
     }
 
-    function handleNavigateToDetail() {
-        navigation.navigate('Detail')
+    function handleNavigateToDetail(id: Number) {
+        navigation.navigate('Detail', { point_id: id})
     }
 
+    function handleSelectItem(id: number) {
+        const alreadySeleted = selectedItems.findIndex(item => item === id);
+        if (alreadySeleted > -1) {
+            const filteredItems = selectedItems.filter(item => item !== id);
+            setSelectedItems(filteredItems)
+        } else {
+            setSelectedItems([...selectedItems, id]);
+        }
+    }
+
+    const [items, setItems] = useState<Item[]>([]);
+    useEffect(() => {
+        api.get('items').then(response => {
+            setItems(response.data);
+        })
+    }, []);
+
+    useEffect(() => {
+        async function loadPosition() {
+            const {status} = await Location.requestPermissionsAsync();
+
+            if (status !== 'granted') {
+                Alert.alert('Ooooops...', 'Precisamos de sua localização');
+                return;
+            }
+
+            const location = await Location.getCurrentPositionAsync();
+
+            const {latitude, longitude} = location.coords;
+            setInitialPosition([
+                latitude, longitude
+            ])
+        }
+
+        loadPosition();
+
+    }, [])
+
+    useEffect(() => {
+        api.get('users', {
+            params: {
+                city: "Santa Rosa do Sul",
+                uf: "SC",
+                items: 1
+            }
+        }).then(response => {
+            setPoints(response.data);
+        })
+    }, [])
+
     return (
-        <SafeAreaView style={{flex:1}}>
-            <View style={styles.container}>
-                <TouchableOpacity onPress={handleNavigateBack}>
-                    <Icon name="arrow-left" size={20} color="#35cb79"/>
-                </TouchableOpacity>
+        <SafeAreaView style={{flex: 1}}>
+            {initialPosition[0] !== 0 && (
+                <View style={styles.container}>
+                    <TouchableOpacity onPress={handleNavigateBack}>
+                        <Icon name="arrow-left" size={20} color="#35cb79"/>
+                    </TouchableOpacity>
 
-                <Text style={styles.title}>
-                    Bem Vindo.
-                </Text>
-                <Text style={styles.description}>
-                    Encontre no mapa um Pet
-                </Text>
+                    <Text style={styles.title}>
+                        Bem Vindo.
+                    </Text>
+                    <Text style={styles.description}>
+                        Encontre no mapa um Pet
+                    </Text>
 
-                <View style={styles.mapContainer}>
-                    <MapView
-                        style={styles.map}
-                        initialRegion={{
-                            latitude: -29.134506,
-                            longitude: -49.715062,
-                            latitudeDelta: 0.014,
-                            longitudeDelta: 0.014,
-                        }}
-                    >
-                        <Marker
-                            style={styles.mapMarker}
-                            onPress={handleNavigateToDetail}
-                            coordinate={{
-                                latitude: -29.134506,
-                                longitude: -49.715062,
+                    <View style={styles.mapContainer}>
+                        <MapView
+                            style={styles.map}
+                            loadingEnabled={initialPosition[0] === 0}
+                            initialRegion={{
+                                latitude: initialPosition[0],
+                                longitude: initialPosition[1],
+                                latitudeDelta: 0.014,
+                                longitudeDelta: 0.014,
                             }}
                         >
-                            <View style={styles.mapMarkerContainer}>
-                                <Image style={styles.mapMarkerImage} source={{uri:'https://images.unsplash.com/photo-1544568100-847a948585b9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80'}}/>
-                                <Text style={styles.mapMarkerTitle}>Doguineo</Text>
-                            </View>
-                        </Marker>
-                    </MapView>
+                            {points.map(point => (
+                                <Marker
+                                    key={String(point.id)}
+                                    style={styles.mapMarker}
+                                    onPress={() => {
+                                        handleNavigateToDetail(point.id)
+                                    }}
+                                    coordinate={{
+                                        latitude: Number(point.latitude),
+                                        longitude: Number(point.longitude),
+                                    }}
+                                >
+                                    <View style={styles.mapMarkerContainer}>
+                                        <Image style={styles.mapMarkerImage}
+                                               source={{uri: point.image}}/>
+                                        <Text style={styles.mapMarkerTitle}>{point.name}</Text>
+                                    </View>
+                                </Marker>
+                            ))}
+                        </MapView>
+                    </View>
                 </View>
-            </View>
+            )}
             <View style={styles.itemsContainer}>
                 <ScrollView
                     horizontal
                     showsHorizontalScrollIndicator={false}
                     contentContainerStyle={{paddingHorizontal: 20}}
                 >
-                    <TouchableOpacity style={styles.item} onPress={() => {
-                    }}>
-                        <Text style={styles.itemTitle}>Pelo Curto</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {
-                    }}>
-                        <Text style={styles.itemTitle}>Pelo Curto</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {
-                    }}>
-                        <Text style={styles.itemTitle}>Pelo Curto</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {
-                    }}>
-                        <Text style={styles.itemTitle}>Pelo Curto</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {
-                    }}>
-                        <Text style={styles.itemTitle}>Pelo Curto</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {
-                    }}>
-                        <Text style={styles.itemTitle}>Pelo Curto</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.item} onPress={() => {
-                    }}>
-                        <Text style={styles.itemTitle}>Pelo Curto</Text>
-                    </TouchableOpacity>
+                    {items.map(item => (
+                        <TouchableOpacity
+                            activeOpacity={0.6}
+                            key={String(item.id)}
+                            style={[
+                                styles.item,
+                                selectedItems.includes(item.id) ? styles.selectedItem : {}
+                            ]}
+                            onPress={() => {
+                                handleSelectItem(item.id)
+                            }}
+                        >
+                            <Text style={styles.itemTitle}>
+                                {item.name}
+                            </Text>
+                        </TouchableOpacity>
+                    ))}
                 </ScrollView>
             </View>
         </SafeAreaView>
